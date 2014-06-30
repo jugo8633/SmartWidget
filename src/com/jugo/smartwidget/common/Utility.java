dc94fe55f0c9982db2f57be03a8be66cac9d85fb
jugo <jugo.tw@gmail.com>
2014-06-30 17:14:39 +0800

    update

--- src/com/jugo/smartwidget/common/Utility.java
+++ src/com/jugo/smartwidget/common/Utility.java
@@ -1,5 +1,8 @@
 package com.jugo.smartwidget.common;
 
+import android.graphics.Rect;
+import android.view.View;
+
 public abstract class Utility
 {
 	private static int	mnUserId	= 2048;
@@ -7,5 +10,20 @@ public abstract class Utility
 	public static int getUserId()
 	{
 		return (++mnUserId);
+	}
+
+	public static Rect getViewRect(View view)
+	{
+		int[] l = new int[2];
+		view.getLocationOnScreen(l);
+		Rect rect = new Rect(l[0], l[1], l[0] + view.getWidth(), l[1] + view.getHeight());
+		l = null;
+		return rect;
+	}
+
+	public static boolean isViewContains(int nX, int nY, View view)
+	{
+		Rect rect = getViewRect(view);
+		return rect.contains(nX, nY);
 	}
 }
--- src/com/jugo/smartwidget/image/BitmapHandler.java
+++ src/com/jugo/smartwidget/image/BitmapHandler.java
@@ -1,7 +1,11 @@
 package com.jugo.smartwidget.image;
 
+import com.jugo.smartwidget.common.Logs;
+
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Rect;
 
 public abstract class BitmapHandler
@@ -99,5 +103,92 @@ public abstract class BitmapHandler
 				pxs[p + x - 1] = sampleColor;
 			}
 		}
+	}
+
+	public static Bitmap AdjustTobMosaic(Bitmap bitmap, int effectWidth)
+	{
+		int nAlpha = 0;
+		//Bitmap bitmap2 = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Config.ARGB_8888);
+		Bitmap bitmap2 = bitmap.copy(Config.ARGB_8888, true);
+		// 差異最多的就是以照一定範圍取樣 玩之後直接去下一個範圍
+		for (int heightOfffset = 0; heightOfffset < bitmap.getHeight(); heightOfffset += effectWidth)
+		{
+			for (int widthOffset = 0; widthOffset < bitmap.getWidth(); widthOffset += effectWidth)
+			{
+				int avgR = 0, avgG = 0, avgB = 0;
+				int blurPixelCount = 0;
+
+				for (int x = widthOffset; (x < widthOffset + effectWidth && x < bitmap.getWidth()); ++x)
+				{
+					for (int y = heightOfffset; (y < heightOfffset + effectWidth && y < bitmap.getHeight()); ++y)
+					{
+						int pixel = bitmap.getPixel(x, y);
+						nAlpha = Color.alpha(pixel);
+						avgR += Color.red(pixel);
+						avgG += Color.green(pixel);
+						avgB += Color.blue(pixel);
+
+						++blurPixelCount;
+					}
+				}
+
+				// 計算範圍平均
+				avgR = avgR / blurPixelCount;
+				avgG = avgG / blurPixelCount;
+				avgB = avgB / blurPixelCount;
+
+				// 所有範圍內都設定此值
+				for (int x = widthOffset; (x < widthOffset + effectWidth && x < bitmap.getWidth()); x++)
+				{
+					for (int y = heightOfffset; (y < heightOfffset + effectWidth && y < bitmap.getHeight()); y++)
+					{
+						//	int pixelAlpha = Color.alpha(0);
+						int red = Color.red(avgR);
+						int green = Color.green(avgG);
+						int blue = Color.blue(avgB);
+						int new_pixel = Color.argb(nAlpha, red, green, blue);
+						Logs.showTrace("a=" + nAlpha + " r=" + red + " g=" + green + " b=" + blue + " pixel="
+								+ new_pixel + " ####################");
+
+						bitmap2.setPixel(x, y, new_pixel);
+
+					}
+				}
+
+			}
+		}
+
+		return bitmap2;
+	}
+
+	public static void releaseBitmap(Bitmap bitmap)
+	{
+		if (null != bitmap)
+		{
+			if (!bitmap.isRecycled())
+			{
+				bitmap.recycle();
+			}
+		}
+		bitmap = null;
+	}
+
+	public static Bitmap scaleBitmap(Bitmap bmpSrc, int quality)
+	{
+		int bitmapWidth = bmpSrc.getWidth();
+		int bitmapHeight = bmpSrc.getHeight();
+		// 缩放图片的尺寸
+		float scaleWidth = (float) quality / bitmapWidth;
+		float scaleHeight = (float) quality / bitmapHeight;
+		Matrix matrix = new Matrix();
+		matrix.postScale(scaleWidth, scaleHeight);
+		// 产生缩放后的Bitmap对象
+		Bitmap resizeBitmap = Bitmap.createBitmap(bmpSrc, 0, 0, bitmapWidth, bitmapHeight, matrix, false);
+
+		if (!bmpSrc.isRecycled())
+		{
+			bmpSrc.recycle();//记得释放资源，否则会内存溢出
+		}
+		return resizeBitmap;
 	}
 }
